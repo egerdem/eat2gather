54492be450b6d64d29c0b3eddfb20e9ddb8a11f8
ege <ege.erdem@gmail.com>
2024-08-06 03:55:05 +0300

    Initial Commit

---
+++ eat2gather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  eat2gather
+//
+//  Created by Ege Erdem on 6.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eat2gather/eat2gatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  eat2gatherApp.swift
+//  eat2gather
+//
+//  Created by Ege Erdem on 6.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct eat2gatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
